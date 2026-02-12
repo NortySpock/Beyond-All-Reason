7e503476911e26b62ea7d49aa3e5e4dd3f2a343d
David Norton <david.k.norton@gmail.com>
2026-02-11 22:26:36 -0600

    ok, that's enough to get the test to emit a replay

--- tools/headless_testing/start.sh
+++ tools/headless_testing/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-rm -rf $1/LuaUI/Config
+mkdir -p "$1/demos"
+mkdir -p "$1/testlog"
+rm -rf "$1/LuaUI/Config"
 $1/engine/*/spring-headless --isolation --write-dir "$1" "$2"
+echo "--- Contents of /bar/demos: ---"
+ls -la "$1/demos"
+echo "--- Contents of /bar/testlog: ---"
+ls -la "$1/testlog"
+echo "--- Contents of /bar/cache: ---"
+ls -la "$1/cache"
+echo "--- Contents of /bar: ---"
+ls -la "$1"
